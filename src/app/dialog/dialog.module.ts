d7fe163d11836905c349b1bce82420cd1310fe65
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-04-16 14:06:46 -0500

    feat: Include directive into modal module

--- src/app/dialog/dialog.module.ts
+++ src/app/dialog/dialog.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogComponent } from './components/dialog/dialog.component';
+import { InsertionDirective } from './directives/insertion.directive';
 
 @NgModule({
   declarations: [
-    DialogComponent
+    DialogComponent,
+    InsertionDirective
   ],
   entryComponents: [
     DialogComponent
