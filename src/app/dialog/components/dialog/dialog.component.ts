eccd8e3d94d388f2859c0216c1fef14849cdcc1f
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-04-16 12:25:41 -0500

    feat: Adding the callbacks to the component

--- src/app/dialog/components/dialog/dialog.component.ts
+++ src/app/dialog/components/dialog/dialog.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, Type} from '@angular/core';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
-export class DialogComponent implements OnInit {
+export class DialogComponent implements AfterViewInit, OnDestroy {
+  childComponentType: Type<any>;
 
-  constructor() { }
-
-  ngOnInit() {
+  ngAfterViewInit() {
   }
 
+  ngOnDestroy() {
+  }
+
+  onOverlayClicked(evt: MouseEvent) {
+    // close the dialog
+  }
+
+  onDialogClicked(evt: MouseEvent) {
+    evt.stopPropagation()
+  }
 }
