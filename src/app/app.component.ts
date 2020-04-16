e7b97f3da737b42670160c7b6604653a1e40297d
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-04-16 18:23:00 -0500

    feat: Use the dialog service to open a dialog with example component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ExampleComponent} from "./example/example.component";
+import {DialogService} from "./dialog/services/dialog.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-dynamic-components';
+  constructor(public dialog: DialogService) {
+    this.dialog.open(ExampleComponent)
+  }
 }
