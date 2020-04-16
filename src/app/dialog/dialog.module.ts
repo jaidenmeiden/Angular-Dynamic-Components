6dc1ccf64d042abbeb849e30a010b517b7039af4
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-04-16 12:09:48 -0500

    feat: Include and export module component into dialog module

--- src/app/dialog/dialog.module.ts
+++ src/app/dialog/dialog.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DialogComponent
+  ],
+  exports: [
+    DialogComponent
+  ],
   imports: [
     CommonModule
   ]
