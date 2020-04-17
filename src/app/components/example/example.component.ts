0a267e17306d05eab7309d1431139c2ff9b1bb62
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-04-16 19:19:15 -0500

    feat: Use data of the config-object in our ExampleComponent

--- src/app/components/example/example.component.ts
+++ src/app/components/example/example.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import {DialogConfig} from "../../dialog/classes/dialog-config";
 
 @Component({
   selector: 'app-example',
@@ -6,5 +7,7 @@ import { Component} from '@angular/core';
   styleUrls: ['./example.component.css']
 })
 export class ExampleComponent {
+  constructor(public config: DialogConfig) {}
+
   onClose() {}
 }
